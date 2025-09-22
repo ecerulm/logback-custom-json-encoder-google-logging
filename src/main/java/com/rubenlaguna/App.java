a65fbaddd7d6d145fb4a66778fd83ec2b9a94ebf
Rubén Laguna <ruben.laguna@gmail.com>
2025-09-22 19:18:11 +0200

    Use Logger

--- src/main/java/com/rubenlaguna/App.java
+++ src/main/java/com/rubenlaguna/App.java
@@ -1,10 +1,16 @@
 package com.rubenlaguna;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Logger logger = LoggerFactory.getLogger("chapters.introduction.HelloWorld1");
+        logger.debug("Hello world.");
+
     }
 }
